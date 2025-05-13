cf9189fd43d335339c3b7ce1fc7d9f05c4331306
lord gavyt <140648649+gavyt@users.noreply.github.com>
2025-05-13 09:12:44 +0000

    sadf

---
+++ index.js
@@ -0,0 +1,94 @@
+let expr = '';
+
+const EEval = (value)=>{
+    console.log(value);;
+;;;;;;;;;;;;;
+;;var LET=0;;
+;;;;;;;;;;;;;
+if  ((((((((((((((((
+    ((((
+        ((
+            ((value.includes('%')))
+        ))))
+    )))))))
+))))))))))){  value.split('').forEach(a =>
+{{{{{
+    
+    console.log("===============")
+    ;;
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ;a       =     value.slice(0, LET-1);
+        ;b=value.slice(LET+1,value.lenght-1);
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        
+        
+        
+        {{{
+            
+            
+            ////Чтобы найти процент от числа, нужно умножить это число на дробь, соответствующую проценту, делённую на 100
+            
+            
+            ;;a= eval(a);;b=eval(b);;
+            ;;  value=(a*b/100)  
+            console.log(value,'fffffffffffffffffffffffffffff')  ;;
+            ; ; ; ; ; ; ; ; ; ; ; ; ;
+                 ;;;;;;;;;;;
+                 ;;;;;;;;;;;
+            ;;;;;;;;;;;;;;;;;;;
+            
+            //////////////////////////;   
+       ;         ;;;;;;;;;           //
+       ;         ;;LET++;;           //
+       ;         ;;;;;;;;;           //
+            //////////////////////////;
+            ;
+
+    }}}
+;;;
+;
+;
+;
+}}}}});;
+}
+{value = eval(value)} 
+    return value
+}
+
+;;const buttons = document.querySelectorAll(".button");;
+;;const display = document.querySelector(".display");;
+buttons.forEach(button => {button.addEventListener('click', (a) => {
+    //if (((button.getAttribute("data-action").includes("%")))) return(((((((((((((null)))))))))))))//
+    if(button.getAttribute("data-action").includes("=")){ 
+        try{display.textContent = expr = EEval(expr)} 
+        catch(eeee){
+            ;;display.textContent = expr ='0',''                                                        ;;
+            prompt("ПОЧЕМУ ТЫ ПОЛЬЗУЕШСЯ ЭТИП ПРИЛОЖЕНИЕМ НЕ ПРАВИЛЬНА А?? ТЫ ЧТО ГЛУПЫЙ? 🤡🗿") == "" 
+            ? prompt("не избегай ответа \n ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧")                                          
+            : null
+        ;;;;
+        console.log(eeee,'<<<<<================================================')    
+        } 
+        return("XOXO SPPPPWWW 🗿");}
+    if(button.getAttribute("data-action").includes("change_sign")) { 
+        
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ;;if((expr[0]!=='-')&&(expr[0]!='+'))/*s_expression_like*/(expr='+'+expr);;
+        ;;arrrrray = expr.split('').map(e => {                                   ;;
+        ;;if((e=='-')){e='+' }else if((e=='+')) e  = '-'                         ;;  
+        ;;return e                                                               ;;
+        ;;})                                                                     ;;
+        ;;expr  = arrrrray.join('')                                              ;;
+        ;;display.textContent    = expr                                          ;;
+        ;;return("09+djjd+javscriptЭ",console.log(" change -/+"))                ;;
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    }
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;          expr      +=      button.getAttribute("data-action") //                                   ;;
+    ;;          display.textContent      =   expr.replaceAll('*','✖')   //✖                              ;;
+    ;;if(button.getAttribute("data-action").includes("delete")){(expr = ''); (display.textContent = '0');};;
+    ;;                                                          console.log("read", expr)})})             ;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    // return e == '-' ? "+" : e == "+" ? "-" : e 
